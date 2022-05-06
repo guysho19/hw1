3edf0bdff1b67e0ae283ad5165100b891a40f2a1
guysho19 <102859035+guysho19@users.noreply.github.com>
2022-05-06 16:27:19 +0300

    Update GamePiece.java

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,11 +1,22 @@
 public class GamePiece {
     private Color color;
+    private int currentSquare;
 
     public GamePiece(Color color) {
+        this.currentSquare=1;
         this.color = color;
     }
 
     public Color getColor() {
         return color;
     }
+
+    public int getCurrentSquare() {
+        return currentSquare;
+    }
+
+    public void setCurrentSquare(int currentSquare) {
+        this.currentSquare = currentSquare;
+    }
+
 }
